df0cb46c66eeb021ee89d23cfe84765f47e459d2
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-01-24 17:47:07 -0700

    fixes for pypi

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyktc',
-    version='0.1',
-    description='A Python implementation of a basic keyword transposition cipher',
+    version='1.0.0',
     author='w4ffl35',
-    packages=find_packages(where='src'),
+    description='A Python implementation of a basic keyword transposition cipher',
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords="cipher, keyword transposition cipher, ktc, transposition cipher",
+    license='MIT',
+    url="https://github.com/w4ffl35/pyktc",
     package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     entry_points={
         'console_scripts': [
             'pyktc = pyktc.main:main',
